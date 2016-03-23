62214068a4c8af84cda53f55e342d921b7500ffe
Benjamin Hepp <benjamin.hepp@inf.ethz.ch>
2016-03-23 16:38:22 +0100

    Fixed uwb tracker node

--- src/nodes/uwb_tracker_node.cpp
+++ src/nodes/uwb_tracker_node.cpp
@@ -17,15 +17,8 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "uwb_tracker");
 
-UWBTracker uwbt;
-ros::Rate r(1);
-//while(ros::ok()){
-    ros::spin();
-    //ros::spinOnce();
-    //uwbt.estimate();
-    //r.sleep();
-//}
-
+  UWBTracker uwb;
+  ros::spin();
 
 return 0;
 }
